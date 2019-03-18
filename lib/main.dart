feff7e68dedf5b6b68ed42ac064e8a3e5ff66a1c
Yurii Zaporozhchenko <yurnero017@gmail.com>
2019-03-18 17:07:31 +0200

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,18 @@
 import 'package:flutter/material.dart';
+import './product_manager.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('EasyList'),
-          ),
-          body: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(5.0),
-                child: RaisedButton(
-                  onPressed: () {
-                    setState(() {
-                      _products.add('Advanced Food Tester');
-                    });
-                  },
-                  child: Text('Add Product'),
-                ),
-              ),
-            ],
-          )),
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductManager(),
+      ),
     );
   }
 }
